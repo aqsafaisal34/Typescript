0e15d43fc45bdae3c3e9fb05bb8ed8be4ced2ffa
aqsafaisal34 <aqsay.faisal34@gmail.com>
2023-03-29 01:23:55 +0500

    push class3 code

--- dist/app.js
+++ dist/app.js
@@ -6,12 +6,30 @@
 // let admin: [number, string] = [1, "Ahsan"];
 // admin.push("Ali");
 //class
+// class Student {
+//     name: string;
+//     rollNo: number;
+//     constructor(n: string, r: number){
+//      this.name = n;
+//      this.rollNo = r;
+//     }
+// }
 class Student {
-    constructor(n, r) {
-        this.name = n;
-        this.rollNo = r;
+    constructor(name, rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.skills = [];
+    }
+    addSkill(skill) {
+        this.skills.push(skill);
     }
 }
 const student1 = new Student("Aqsa", 1234);
+// student1.skills.push("Javascript");
+student1.addSkill("Javascript");
+// student1.rollNo = 567
 console.log(student1);
+const student2 = new Student("Tasmia", 1235);
+student2.addSkill("Typescript");
+console.log(student2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,33 @@
 
 //class
 
+// class Student {
+//     name: string;
+//     rollNo: number;
+//     constructor(n: string, r: number){
+//      this.name = n;
+//      this.rollNo = r;
+//     }
+// }
+
 class Student {
-    name: string;
-    rollNo: number;
-    constructor(n: string, r: number){
-     this.name = n;
-     this.rollNo = r;
+    private skills: string[] = [];
+    constructor(public name: string, public readonly rollNo: number){
+        
+    }
+    addSkill(skill: string){
+        this.skills.push(skill);
     }
 }
 
 const student1 = new Student("Aqsa", 1234);
-console.log(student1)
+// student1.skills.push("Javascript");
+student1.addSkill("Javascript");
+// student1.rollNo = 567
+console.log(student1);
+
+const student2 = new Student("Tasmia", 1235);
+student2.addSkill("Typescript");
+console.log(student2);
+
+
